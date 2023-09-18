a3b21f4f8a3ff996682dcdd45b41fab4d79f66e6
shinjiokita <monariza7423@gmail.com>
2023-09-18 17:00:48 +0900

    contact　api保存

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -14,7 +14,6 @@ class ContactController extends Controller
             'last_name' => 'required|string|max:255',
             'first_name_kana' => 'required|string|max:255',
             'last_name_kana' => 'required|string|max:255',
-            'company' => 'required|string|max:255',
             'email' => 'required|string|max:255',
             'contact_type' => 'required|in:事業(WEB制作・開発)について,事業(オハナスタイル)について,事業(リモートスタイル)について,採用について,その他',
             'content' => 'required',
